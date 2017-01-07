35d0157c3d446bef72bff5da1ace7e2cbdcd91a5
Veselin Stoyanov <veselin1996@gmail.com>
2017-01-07 19:43:13 +0200

    feature/f2017050102-project-structure - Created initial project structure - Planned and created files - Added "gomasd" folder planned for the final executable - Fixed pre-commit script - Fixed .travis.yml pre commit script path - Beautified pre commit script output

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
 
 GOFMT_FILES=$(gofmt -l .)
+printf >&2 'Running "gofmt -l" ...\n'
 if [ -n "${GOFMT_FILES}" ]; then
-  printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILES}"
+  printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILES}\n"
   exit 1
+else
+  printf >&2 'No formatting errors were found.\n'
 fi
+echo "------------------------------------------\n"
 
 GOLINT_ERRORS=$(golint ./... | grep -v "Id should be")
+printf >&2 'Running "golint ./..." ...\n'
 if [ -n "${GOLINT_ERRORS}" ]; then
-  printf >&2 'golint failed for the following reasons:\n%s\n\nplease run 'golint ./...' on your changes before committing.\n' "${GOLINT_ERRORS}"
+  printf >&2 'golint failed for the following reasons:\n%s\n\nplease run 'golint ./...' on your changes before committing.\n' "${GOLINT_ERRORS}\n"
   exit 1
+else
+  printf >&2 'No style mistakes were found.\n'
 fi
+echo "------------------------------------------\n"
 
-GOVET_ERRORS=$(go tool vet -copylocks=false *.go 2>&1)
+GOVET_ERRORS=$(go tool vet ./ 2>&1)
+printf >&2 'Running "go tool vet ./" ...\n'
 if [ -n "${GOVET_ERRORS}" ]; then
-  printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet *.go" on your changes before committing.\n' "${GOVET_ERRORS}"
+  printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet ./" on your changes before committing.\n' "${GOVET_ERRORS}\n"
+  exit 1
+else
+  printf >&2 'No suspicious constructs were found.\n'
+fi
+echo "------------------------------------------\n"
+
+printf >&2 'Running "go test -v ./..." ...\n\n'
+go test -v ./...
+if [ $? -eq 0 ]; then
+  printf >&2 '\nAll tests passed successfully.\n'
+else
+  printf >&2 '\ngo test failed, please fix before committing.\n'
   exit 1
 fi
-
-if [ -z "${NOTEST}" ]; then
-  printf >&2 'Running short tests...\n'
-  env AMQP_URL= go test -short -v | egrep 'PASS|ok'
-
-  if [ $? -ne 0 ]; then
-    printf >&2 'go test failed, please fix before committing.\n'
-    exit 1
-  fi
-fi
+echo "------------------------------------------\n"
